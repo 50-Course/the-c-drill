6c397a1fb803f31eb8dc1f73764dabaf656ca8bb
50-Course <erioluwaadeodu@my.uopeople.edu>
2023-01-02 13:17:54 +0100

    :construction: temperatures table

---
+++ temperatures.c
@@ -0,0 +1,30 @@
+/*
+Display a table of Farenhenit-Celcius temperatures.
+
+*/
+
+#include <stdio.h>
+
+int main(int argc, int *argv[])
+{
+   // prints table for temperatures
+   // in range 0, 20, ..., 300
+
+   int celcius, fahr;
+   int lower, upper, step;
+
+   lower  = 0; /* lower limit of temperature table */
+   upper  = 300; /* upper limit of temperature table */
+   step = 20; /* step size */
+
+   fahr = lower; // sets the current temp to curr step
+
+   while(fahr <= upper)
+   {
+        celcius = 5 * (fahr - 32) / 9;
+        printf("%d\t%d\n", fahr, celcius);
+
+        // increment the step
+        fahr = fahr + step;
+   }
+}
